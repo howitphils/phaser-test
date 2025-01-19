02979cf792af7096860e5b9ae2b6039cf9047166
howitphils <uladzislauhr@gmail.com>
2025-01-19 13:13:41 +0300

    first one

---
+++ server.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+// Define MIME types for different file extensions
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".gif": "image/gif",
+};
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  // Get file path
+  let filePath =
+    req.url === "/" // If the URL is "/", serve index.html from the public folder
+      ? path.join(__dirname, "public", "index.html")
+      : path.join(__dirname, req.url); // Otherwise, serve the requested file via the URL (e.g. /assets/image.png)
+
+  // Get file extension
+  const extname = path.extname(filePath); // e.g. .html, .js, .css, .png, .jpg, .gif
+  const contentType = mimeTypes[extname] || "application/octet-stream";
+
+  // Create read stream
+  const stream = fs.createReadStream(filePath);
+
+  // Handle errors
+  stream.on("error", (error) => {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>404 Not Found</h1>");
+  });
+
+  // Serve the file
+  res.writeHead(200, { "Content-Type": contentType });
+  stream.pipe(res);
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
